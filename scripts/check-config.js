2ca5248989f811bb107a9fbf47fcbf795399a1bd
CiobotaruAndrei <big.master.7771@gmail.com>
2026-05-15 19:21:06 +0300

    Add config validation script

---
+++ scripts/check-config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const path = require("path");
+const { validateConfig } = require("../configValidator");
+
+const configPath = path.resolve(process.cwd(), process.env.CONFIG_PATH || "./config.json");
+const config = require(configPath);
+const validated = validateConfig(config, configPath);
+
+console.log(`Config OK: ${validated.games.length} entries from ${configPath}`);
