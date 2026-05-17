bd0ba488820e9039aa579630e3385fec2e5fcd1d
CiobotaruAndrei <big.master.7771@gmail.com>
2026-05-17 23:14:07 +0300

    Convert health metrics to TypeScript

---
+++ src/app/health/metrics.ts
@@ -0,0 +1,19 @@
+import type { BotMetrics } from "../../types";
+
+function createMetrics(): BotMetrics {
+  return {
+    fetchSuccess: 0,
+    fetchFail: 0,
+    httpRetries: 0,
+    rateLimitHits: 0,
+    cronRuns: 0,
+    cronErrors: 0,
+    cronSkippedDueToLock: 0,
+    cronSkippedDueToHealth: 0,
+    cronAborted: 0,
+    httpRateLimitDrops: 0,
+    startedAt: Date.now()
+  };
+}
+
+export { createMetrics };
