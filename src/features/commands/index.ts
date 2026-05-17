3a9520c8449b4b8613442f8a334648c1a99b4092
CiobotaruAndrei <big.master.7771@gmail.com>
2026-05-17 23:21:30 +0300

    Convert command entrypoint to TypeScript

---
+++ src/features/commands/index.ts
@@ -0,0 +1,28 @@
+const ctx = require("./runtime") as any;
+
+require("./cache")(ctx);
+require("../../domain/deals/filters")(ctx);
+require("./ui")(ctx);
+require("../notifications")(ctx);
+require("./slashCommands")(ctx);
+require("./interactions")(ctx);
+
+const commands = {
+  startCacheCleaner: ctx.startCacheCleaner,
+  cleanCache: ctx.cleanCache,
+  getCacheSizes: ctx.getCacheSizes,
+  setGlobalCacheTtl: ctx.setGlobalCacheTtl,
+  checkForUpdates: ctx.checkForUpdates,
+  checkForDiscounts: ctx.checkForDiscounts,
+  buildOptimizedGameList: ctx.buildOptimizedGameList,
+  registerSlashCommands: ctx.registerSlashCommands,
+  buildSlashCommandDefinitions: ctx.buildSlashCommandDefinitions,
+  handleInteraction: ctx.handleInteraction,
+  buildHelpEmbed: ctx.buildHelpEmbed,
+  findGameAndSuggestion: ctx.findGameAndSuggestion,
+  getFindGameCacheSize: ctx.getFindGameCacheSize,
+  clearFindGameCache: ctx.clearFindGameCache,
+  formatUserError: ctx.formatUserError
+};
+
+export = commands;
