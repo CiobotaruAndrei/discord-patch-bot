f578f512ab9d749ac33f6277d9167ba15984a835
CiobotaruAndrei <big.master.7771@gmail.com>
2026-05-18 15:23:50 +0300

    Convert remaining code to TypeScript

--- src/infra/mongo/runtime.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const mongoose = require("mongoose");
-const crypto = require("crypto");
-const axios = require("axios");
-const { z } = require("zod");
-const { AsyncLocalStorage } = require("async_hooks");
-
-module.exports = { mongoose, crypto, axios, z, AsyncLocalStorage };
