2b0fccf9a5a34c6d2dab8fc42d88ad64cba6b9e7
CiobotaruAndrei <big.master.7771@gmail.com>
2026-05-18 15:27:11 +0300

    Convert remaining code to TypeScript

---
+++ src/sources/runtime.ts
@@ -0,0 +1,18 @@
+"use strict";
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+const Parser = require("rss-parser");
+const crypto = require("crypto");
+const data = require("../infra/mongo");
+
+module.exports = {
+  axios,
+  cheerio,
+  Parser,
+  crypto,
+  rssParser: new Parser(),
+  ...data
+};
+
+export {};
