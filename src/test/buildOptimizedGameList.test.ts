75679fec32a24d07b439ad927f7e5f7339ddb6d4
CiobotaruAndrei <big.master.7771@gmail.com>
2026-05-20 22:52:10 +0300

    Update optimized game list test import for commandRegistry

--- src/test/buildOptimizedGameList.test.ts
+++ src/test/buildOptimizedGameList.test.ts
@@ -8,7 +8,7 @@ process.env.METRICS_PUBLIC ||= "true";
 
 const test = require("node:test");
 const assert = require("node:assert/strict");
-const { buildOptimizedGameList } = require("../features/commands");
+const { buildOptimizedGameList } = require("../features/commands/commandRegistry");
 
 const allGames = [
   { key: "cs2", name: "CS2", type: "steam", appId: "730" },
