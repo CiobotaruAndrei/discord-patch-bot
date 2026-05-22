6cdba44173e6a94032ba6dbd29dac7f737820aa7
CiobotaruAndrei <big.master.7771@gmail.com>
2026-05-22 19:21:20 +0300

    Add functional command router facade

---
+++ src/features/command-router/legacyInteractionRouter.ts
@@ -0,0 +1,5 @@
+"use strict";
+
+// Compatibility adapter while the remaining /latest, /dlc, /status and autocomplete
+// paths are extracted from the legacy command file into dedicated handlers.
+export = require("../commands/interactions");
