9582346ec6c7deb581cb1fe283a5a8bc3b828209
CiobotaruAndrei <big.master.7771@gmail.com>
2026-07-11 21:14:10 +0300

    test: cover guild settings event fanout

---
+++ src/test/guildSettingsEvents.test.ts
@@ -0,0 +1,22 @@
+import test = require("node:test");
+import assert = require("node:assert/strict");
+import { publishGuildSettingsChanged, subscribeGuildSettingsChanged } from "../infra/mongo/guildSettingsEvents";
+
+test("guild settings changed notifica toti abonatii cu guild-ul scris", () => {
+  const calls: string[] = [];
+  const unsubscribeFirst = subscribeGuildSettingsChanged(guildId => calls.push(`first:${guildId}`));
+  const unsubscribeSecond = subscribeGuildSettingsChanged(guildId => calls.push(`second:${guildId}`));
+  publishGuildSettingsChanged("guild-1");
+  unsubscribeFirst();
+  unsubscribeSecond();
+  assert.deepEqual(calls, ["first:guild-1", "second:guild-1"]);
+});
+
+test("unsubscribe opreste invalidarile ulterioare", () => {
+  const calls: string[] = [];
+  const unsubscribe = subscribeGuildSettingsChanged(guildId => calls.push(guildId));
+  unsubscribe();
+  publishGuildSettingsChanged("guild-2");
+  assert.deepEqual(calls, []);
+});
+
